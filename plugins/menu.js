c38694610c62ee5ff758ede6fca890d8dc6bb3d0
sunooiox-glitch <sunooiox@gmail.com>
2026-06-04 06:27:41 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -30,19 +30,19 @@ const handler = async (m, { conn, usedPrefix: _p, command, isOwner, args }) => {
 
 	const defaultMenu = {
 		before: `
-╭━━━〔 ${conn.user.name} 〕━━━⬣
+┏━━━━━━━ ${conn.user.name}
 ┃ 👋 ${ucapan()}, %name
 ┃
 ┃ 📅 %week, %date
-┃ ⏱ Uptime: %uptime
-┃ 👥 Users: %rtotalreg/%totalreg
-╰━━━━━━━━━━━━⬣
+┃ Uptime : %uptime
+┃ Users : %rtotalreg/%totalreg
+┗━━━━━━━━━━━
 %readmore`.trim(),
 
-		header: '\n╭─〔 %category 〕',
-		body: '│ ◦ %cmd %flags',
-		footer: '╰────────────⬣',
-		after: '',
+		header: '\n┏━━━━━━ %category ↓',
+        body: '┃ • %cmd %flags',
+        footer: '┗━━━━━━━━━━━━',
+        after: '',
 	}
 
 	try {
